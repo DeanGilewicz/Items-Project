d36a1d75dcb85e13f744137666eaf10fda137800
Dean Gilewicz <gilewiczd@gmail.com>
2014-10-01 15:09:12 -0400

    calculated average price of items

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log(items);
+/****************************************************************************************************************
+Show me how to calculate the average price of all items.
+
+Please console.log the average. The average price is $23.62
+
+*****************************************************************************************************************/
+
+// create an array to store list of prices of each item
+var priceArray = [];
+
+// call a function to store price of each item from item list into my array
+var priceList = items.forEach (function(x) {
+  priceArray.push(x.price);
+});
+
+// total the prices of each item that are in my array and store in a diferent variable
+var total = priceArray.reduce(function(a, b) {
+  return a + b;
+});
+
+// divide the total price by the number of items in my array
+var avg = total / priceArray.length;
+
+// convert the output to only show to 2 decimal places
+console.log("The average price is $" + avg.toFixed(2));
+
+/****************************************************************************************************************
+Show me how to calculate the average price of all items.
+
+Please console.log the average. The average price is $23.62
+
+*****************************************************************************************************************/
